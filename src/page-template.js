103a10efd80f51d2833abb60affe5827637b2dc3
kodapassey <kodapassey@charter.net>
2022-06-28 16:04:53 -0700

    import test

--- src/page-template.js
+++ src/page-template.js
@@ -1,5 +1,7 @@
-module.exports = (name, github) => {
-    return `
+module.exports = (templateData) => {
+  console.log(templateData);
+
+  return `
     <!DOCTYPE html>
     <html lang="en">
     
@@ -11,8 +13,8 @@ module.exports = (name, github) => {
     </head>
     
     <body>
-      <h1>${name}</h1>
-      <h2><a href="https://github.com/${github}">Github</a></h2>
+      <h1>${templateData.name}</h1>
+      <h2><a href="https://github.com/${templateData.github}">Github</a></h2>
     </body>
     </html>
     `;
